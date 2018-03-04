49edbd40eedc4357382c3c4801bb82a1f77ee98a
mcc1874 <mcc1874@gmial.com>
2018-03-05 05:34:31 +0800

    1

--- ubuntu-php/安装.bat
+++ ubuntu-php/安装.bat
@@ -1,6 +1,6 @@
 @echo off
-set "default_box_name=ubuntu1604-0.1.0"
-set /p "box_name=please input box name(default:ubuntu1604-0.1.0): "
+set "default_box_name=ubuntu1604php-0.1.0"
+set /p "box_name=please input box name(default:ubuntu1604php-0.1.0): "
 
 if not defined box_name (
     set "box_name=%default_box_name%"
